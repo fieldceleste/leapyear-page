112ccce9e3f8b6562ccdd7beb376da034898a592
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-01 11:22:30 -0700

    add in spec 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
-//<--Back End-->
-$(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
+//<--Front End-->
+$(document).ready(function () {
+  $("form#leap-year").submit(function (event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
-    var leapYear = function(year) {
-      return false;
-//<--Front End-->
 
+    //<--Back End-->
+    var leapYear = function(year) {
+      if (year % 4 === 0) {
+        return true;
+      } else {
+        return false;
+      }
     };
+  
   });
 });
